1) Run simulation - see if coordinate level noise in judgments can be fit by a decision model only taking into account comparison noise. 2) CORRECT CALC of LL- not averaging choice prob across contexts anymore

analysis/model_fitting/minimization.py
@@ -8,17 +8,17 @@ LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.INFO)
 
 
-def calculate_gradient(costfunc, vector, pair_a, pair_b, counts, params, vector_length, delta=1e-03):
-    baseline_loss = costfunc(vector, pair_a, pair_b, counts, params)
+def calculate_gradient(costfunc, vector, pair_a, pair_b, counts, repeats, params, vector_length, delta=1e-03):
+    baseline_loss = costfunc(vector, pair_a, pair_b, counts, repeats, params)
     deltas = eye(vector_length) * delta
     vectors = tile(vector.reshape(vector_length, 1), (1, vector_length))
     delta_vectors = vectors + deltas
-    new_loss_vector = apply_along_axis(costfunc, 0, delta_vectors, pair_a, pair_b, counts, params)
+    new_loss_vector = apply_along_axis(costfunc, 0, delta_vectors, pair_a, pair_b, counts, repeats, params)
     gradient = new_loss_vector - baseline_loss
     return gradient
 
 
-def gradient_descent(costfunc, start, pair_a, pair_b, counts, params):
+def gradient_descent(costfunc, start, pair_a, pair_b, counts, repeats, params):
     """"
     Implements stochastic gradient descent.
     The code below is my first attempt and not an optimized program
@@ -31,7 +31,7 @@ def gradient_descent(costfunc, start, pair_a, pair_b, counts, params):
     for _ in range(params['max_iterations']):
         # print(_)
         diff = -params['learning_rate'] * calculate_gradient(costfunc, vector, pair_a, pair_b,
-                                                             counts, params, vector_length)
+                                                             counts, repeats, params, vector_length)
         if np.all(np.abs(diff) <= params['tolerance']):
             print("Stopped on Iteration number {}".format(_ + 1))
             break

analysis/model_fitting/model_fitting.py
@@ -71,19 +71,42 @@ if __name__ == '__main__':
 
     for ii in range(ITERATIONS):
         # read json file into dict
-        pairwise_comparison_responses_by_trial = decompose_similarity_judgments(FILEPATH)
+        with open(FILEPATH) as file:
+            ranking_responses_by_trial = json.load(file)
+
+        # break up ranking responses into pairwise judgments
+        pairwise_comparison_responses = {}
+        pairwise_comparison_num_repeats = {}
+        for config in ranking_responses_by_trial:
+            comparisons, num_repeats = ranking_to_pairwise_comparisons(all_distance_pairs(config),
+                                                                       ranking_responses_by_trial[config]
+                                                                       )
+            for key, count in comparisons.items():
+                pairs = key.split('<')
+                stim1, stim2 = pairs[1].split(',')
+                stim3, stim4 = pairs[0].split(',')
+                new_key = ((NAMES_TO_ID[stim1], NAMES_TO_ID[stim2]), (NAMES_TO_ID[stim3], NAMES_TO_ID[stim4]))
+                if new_key not in pairwise_comparison_responses:
+                    pairwise_comparison_responses[new_key] = count
+                    pairwise_comparison_num_repeats[new_key] = num_repeats[key]
+                else:
+                    # if the comparison is repeated in two trials (context design side-effect)
+                    pairwise_comparison_responses[new_key] += count
+                    pairwise_comparison_num_repeats[new_key] += num_repeats[key]
         # get MDS starting coordinates
-        D = mds.format_distances(mds.heuristic_distances(pairwise_comparison_responses_by_trial, 5))
+        D = mds.format_distances(mds.heuristic_distances(
+            pairwise_comparison_responses, pairwise_comparison_num_repeats))
         coordinates2d, stress = smacof(D, n_components=2, metric=True, eps=1e-9)
 
         # only consider a subset of trials
-        if CONFIG['max_trials'] < len(pairwise_comparison_responses_by_trial):
-            indices = random.sample(pairwise_comparison_responses_by_trial.keys(), CONFIG['max_trials'])
-            subset = {key: pairwise_comparison_responses_by_trial[key] for key in indices}
+        if CONFIG['max_trials'] < len(pairwise_comparison_responses):
+            indices = random.sample(pairwise_comparison_responses.keys(), CONFIG['max_trials'])
+            subset = {key: pairwise_comparison_responses[key] for key in indices}
         else:
-            subset = pairwise_comparison_responses_by_trial
+            subset = pairwise_comparison_responses
 
         # initialize results dataframe
+        total_num_triads = sum([pairwise_comparison_num_repeats[k] for k in subset.keys()])
         result = {'Model': [], 'Log Likelihood': [], 'number of points': [],
                   'Experiment': [EXP] * (2 + len(CONFIG['model_dimensions'])),
                   'Subject': [SUBJECT] * (2 + len(CONFIG['model_dimensions'])),
@@ -92,10 +115,10 @@ if __name__ == '__main__':
         # MODELING WITH DIFFERENT EUCLIDEAN MODELS ###################################################
         num_trials = len(subset)
         for dim in CONFIG['model_dimensions']:
-            LOG.info('#######  {} dimensional geometry'.format(dim))
+            LOG.info('#######  {} dimensional model'.format(dim))
             model_name = str(dim) + 'D'
             CONFIG['n_dim'] = dim
-            x, ll_nd, fmin_costs = rs.points_of_best_fit(subset, CONFIG)
+            x, ll_nd = rs.points_of_best_fit(subset, pairwise_comparison_num_repeats, CONFIG)
             LOG.info("Points: ")
             print(x)
             outfilename = '{}/{}_{}_anchored_points_sigma_{}_dim_{}'.format(
@@ -107,108 +130,109 @@ if __name__ == '__main__':
             np.save(outfilename, x)
             LOG.info("Distances: ")
             distances = pdist(x)
-            ll_nd = -ll_nd / float(num_trials * CONFIG['num_repeats'])
+            ll_nd = -ll_nd / float(total_num_triads)
+            LOG.info('####### LL: {}'.format(np.round(ll_nd, 4)))
             result['Model'].append(model_name)
             result['Log Likelihood'].append(ll_nd)
             result['number of points'].append(CONFIG['num_stimuli'])
             result['Curvature'].append('')
-        # the ii for loop can be taken out later. just need it for a plot
-        #   plt.plot(fmin_costs)
-        # plt.show()
+            # the ii for loop can be taken out later. just need it for a plot
+            #   plt.plot(fmin_costs)
+            # plt.show()
 
-        if CONFIG['hyperbolic']:
-            # HYPERBOLIC MODELS #########################################################################
-            hyp_dim = 2
-            # FIRST OPTIMIZE CURVATURE ###############################
-            MAX_ITERATIONS = CONFIG['max_iterations']
-            INITIAL_ITERATIONS = 2000
-            CONFIG['max_iterations'] = INITIAL_ITERATIONS
-            max_ll = -np.inf
-            c_max_ll = None
-            hyp_start = None
-            for c in np.arange(0.0001, CONFIG['curvature']+0.1, 0.05):
-                CONFIG['curvature'] = c
-                LOG.info('Fitting hyperbolic model with parameters: ')
-                LOG.info('######################################### Hyperbolic model with curvature {}'.format(
-                    str(CONFIG['curvature'])))
-                CONFIG['n_dim'] = hyp_dim
-                start, ll_nd, fmin_costs = rs.hyperbolic_points_of_best_fit(subset, CONFIG)
-                ll_nd = -ll_nd / float(num_trials * CONFIG['num_repeats'])
-                LOG.info('######################################## Log likelihood: {}'.format(str(ll_nd)))
-                if ll_nd > max_ll:
-                    max_ll = ll_nd
-                    c_max_ll = c
-                    hyp_start = start
-                    LOG.info("#### Best curvature so far: " + str(c))
-            # NEXT OPTIMIZE COORDINATES ##############################
-            CONFIG['curvature'] = c_max_ll
-            CONFIG['max_iterations'] = MAX_ITERATIONS - INITIAL_ITERATIONS
-            LOG.info('Fitting hyperbolic model with parameters: ')
-            LOG.info('################################### Hyperbolic model being optimized with curvature {}'.format(
-                str(CONFIG['curvature'])))
-            solution, ll_nd, fmin_costs = rs.hyperbolic_points_of_best_fit(subset, CONFIG, hyp_start)
-            ll_nd = -ll_nd / float(num_trials * CONFIG['num_repeats'])
-            result['Model'].append(str(hyp_dim) + 'D-hyp')
-            result['Log Likelihood'].append(ll_nd)
+            # Deprecated - Feb 1, 2023 - use bootstrap.curvature_and_model_goodness on homePC for curvature LLs
+            # if CONFIG['hyperbolic']:
+            #     # HYPERBOLIC MODELS #########################################################################
+            #     hyp_dim = 2
+            #     # FIRST OPTIMIZE CURVATURE ###############################
+            #     MAX_ITERATIONS = CONFIG['max_iterations']
+            #     INITIAL_ITERATIONS = 2000
+            #     CONFIG['max_iterations'] = INITIAL_ITERATIONS
+            #     max_ll = -np.inf
+            #     c_max_ll = None
+            #     hyp_start = None
+            #     for c in np.arange(0.0001, CONFIG['curvature'] + 0.1, 0.05):
+            #         CONFIG['curvature'] = c
+            #         LOG.info('Fitting hyperbolic model with parameters: ')
+            #         LOG.info('######################################### Hyperbolic model with curvature {}'.format(
+            #             str(CONFIG['curvature'])))
+            #         CONFIG['n_dim'] = hyp_dim
+            #         start, ll_nd, fmin_costs = rs.hyperbolic_points_of_best_fit(subset, CONFIG)
+            #         ll_nd = -ll_nd / float(num_trials * CONFIG['num_repeats'])
+            #         LOG.info('######################################## Log likelihood: {}'.format(str(ll_nd)))
+            #         if ll_nd > max_ll:
+            #             max_ll = ll_nd
+            #             c_max_ll = c
+            #             hyp_start = start
+            #             LOG.info("#### Best curvature so far: " + str(c))
+            #     # NEXT OPTIMIZE COORDINATES ##############################
+            #     CONFIG['curvature'] = c_max_ll
+            #     CONFIG['max_iterations'] = MAX_ITERATIONS - INITIAL_ITERATIONS
+            #     LOG.info('Fitting hyperbolic model with parameters: ')
+            #     LOG.info(
+            #         '################################### Hyperbolic model being optimized with curvature {}'.format(
+            #             str(CONFIG['curvature'])))
+            #     solution, ll_nd, fmin_costs = rs.hyperbolic_points_of_best_fit(subset, CONFIG, hyp_start)
+            #     ll_nd = -ll_nd / float(num_trials * CONFIG['num_repeats'])
+            #     result['Model'].append(str(hyp_dim) + 'D-hyp')
+            #     result['Log Likelihood'].append(ll_nd)
+            #     result['number of points'].append(CONFIG['num_stimuli'])
+            #     result['Experiment'].append(EXP)
+            #     result['Subject'].append(SUBJECT)
+            #     result['Curvature'].append(np.round(c_max_ll, 2))
+            #
+            # if CONFIG['spherical']:
+            #     # SPHERICAL MODELS #########################################################################
+            #     sph_dim = 2
+            #     # FIRST OPTIMIZE CURVATURE ###############################
+            #     MAX_ITERATIONS = CONFIG['max_iterations']
+            #     INITIAL_ITERATIONS = 2000
+            #     CONFIG['max_iterations'] = INITIAL_ITERATIONS
+            #     max_ll = -np.inf
+            #     c_max_ll = None
+            #     sph_start = None
+            #     for c in np.arange(0.0001, ORIGINAL_CURVATURE + 0.1, 0.05):
+            #         CONFIG['curvature'] = c
+            #         LOG.info('Fitting spherical model with parameters: ')
+            #         LOG.info('######################################### Spherical model with curvature {}'.format(
+            #             str(CONFIG['curvature'])))
+            #         CONFIG['n_dim'] = sph_dim
+            #         start2, ll_nd, fmin_costs = rs.spherical_points_of_best_fit(subset, CONFIG)
+            #         ll_nd = -ll_nd / float(num_trials * CONFIG['num_repeats'])
+            #         LOG.info('######################################## Log likelihood: {}'.format(str(ll_nd)))
+            #         if ll_nd > max_ll:
+            #             max_ll = ll_nd
+            #             sph_start = start2
+            #             c_max_ll = c
+            #             LOG.info("#### Best curvature so far: " + str(c))
+            #     # NEXT OPTIMIZE COORDINATES ##############################
+            #     CONFIG['curvature'] = c_max_ll
+            #     CONFIG['max_iterations'] = MAX_ITERATIONS - INITIAL_ITERATIONS
+            #     LOG.info('Fitting spherical model with parameters: ')
+            #     LOG.info('################################### Spherical model being optimized with curvature {}'.format(
+            #         str(CONFIG['curvature'])))
+            #     solution, ll_nd, fmin_costs = rs.spherical_points_of_best_fit(subset, CONFIG, sph_start)
+            #     ll_nd = -ll_nd / float(num_trials * CONFIG['num_repeats'])
+            #     result['Model'].append(str(sph_dim) + 'D-sph')
+            #     result['Log Likelihood'].append(ll_nd)
+            #     result['number of points'].append(CONFIG['num_stimuli'])
+            #     result['Experiment'].append(EXP)
+            #     result['Subject'].append(SUBJECT)
+            #     result['Curvature'].append(np.round(c_max_ll, 2))
+
+            # RANDOM AND BEST MODELS ####################################################################
+            ll_best = an.best_model_ll(
+                subset, pairwise_comparison_num_repeats)[0] / float(total_num_triads)
+            result['Model'].append('best')
+            result['Log Likelihood'].append(ll_best)
             result['number of points'].append(CONFIG['num_stimuli'])
-            result['Experiment'].append(EXP)
-            result['Subject'].append(SUBJECT)
-            result['Curvature'].append(np.round(c_max_ll, 2))
-
-        if CONFIG['spherical']:
-            # SPHERICAL MODELS #########################################################################
-            sph_dim = 2
-            # FIRST OPTIMIZE CURVATURE ###############################
-            MAX_ITERATIONS = CONFIG['max_iterations']
-            INITIAL_ITERATIONS = 2000
-            CONFIG['max_iterations'] = INITIAL_ITERATIONS
-            max_ll = -np.inf
-            c_max_ll = None
-            sph_start = None
-            for c in np.arange(0.0001, ORIGINAL_CURVATURE + 0.1, 0.05):
-                CONFIG['curvature'] = c
-                LOG.info('Fitting spherical model with parameters: ')
-                LOG.info('######################################### Spherical model with curvature {}'.format(
-                    str(CONFIG['curvature'])))
-                CONFIG['n_dim'] = sph_dim
-                start2, ll_nd, fmin_costs = rs.spherical_points_of_best_fit(subset, CONFIG)
-                ll_nd = -ll_nd / float(num_trials * CONFIG['num_repeats'])
-                LOG.info('######################################## Log likelihood: {}'.format(str(ll_nd)))
-                if ll_nd > max_ll:
-                    max_ll = ll_nd
-                    sph_start = start2
-                    c_max_ll = c
-                    LOG.info("#### Best curvature so far: " + str(c))
-            # NEXT OPTIMIZE COORDINATES ##############################
-            CONFIG['curvature'] = c_max_ll
-            CONFIG['max_iterations'] = MAX_ITERATIONS - INITIAL_ITERATIONS
-            LOG.info('Fitting spherical model with parameters: ')
-            LOG.info('################################### Spherical model being optimized with curvature {}'.format(
-                str(CONFIG['curvature'])))
-            solution, ll_nd, fmin_costs = rs.spherical_points_of_best_fit(subset, CONFIG, sph_start)
-            ll_nd = -ll_nd / float(num_trials * CONFIG['num_repeats'])
-            result['Model'].append(str(sph_dim) + 'D-sph')
-            result['Log Likelihood'].append(ll_nd)
+            result['Curvature'].append('')
+            ll_random = an.random_choice_ll(
+                subset, pairwise_comparison_num_repeats)[0] / float(total_num_triads)
+            result['Model'].append('random')
+            result['Log Likelihood'].append(ll_random)
             result['number of points'].append(CONFIG['num_stimuli'])
-            result['Experiment'].append(EXP)
-            result['Subject'].append(SUBJECT)
-            result['Curvature'].append(np.round(c_max_ll, 2))
-
-        # RANDOM AND BEST MODELS ####################################################################
-        LOG.info('#######  Random and best geometry')
-        ll_best = an.best_model_ll(
-            subset, CONFIG)[0] / float(num_trials * CONFIG['num_repeats'])
-        result['Model'].append('best')
-        result['Log Likelihood'].append(ll_best)
-        result['number of points'].append(CONFIG['num_stimuli'])
-        result['Curvature'].append('')
-        ll_random = an.random_choice_ll(
-            subset, CONFIG)[0] / float(num_trials * CONFIG['num_repeats'])
-        result['Model'].append('random')
-        result['Log Likelihood'].append(ll_random)
-        result['number of points'].append(CONFIG['num_stimuli'])
-        result['Curvature'].append('')
-
+            result['Curvature'].append('')
         # OUTPUT RESULTS ###############################################################################
         data_frame = pd.DataFrame(result)
         sigma = CONFIG['sigmas']['compare'] + CONFIG['sigmas']['dist']

analysis/model_fitting/pairwise_likelihood_analysis.py
@@ -19,6 +19,7 @@ But if sigma_point > 0, then the noise is the square root of a sum of squares of
 Gaussian distributions, which is not exactly Gaussian. So erf is merely an approximation of the probability,
 since it only accounts for Gaussian sources of noise.
 """
+import yaml
 import logging
 import numpy as np
 from numpy import sqrt, zeros, concatenate, log2
@@ -27,9 +28,10 @@ from scipy.spatial.distance import pdist, squareform
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
-# with open('./analysis/config.yaml', "r") as stream:
-#     data = yaml.safe_load(stream)
-#     EPSILON = float(data['epsilon'])
+
+with open('./analysis/config.yaml', "r") as stream:
+    data = yaml.safe_load(stream)
+    EPSILON = float(data['epsilon'])
 
 
 def params_to_points(x0, num_stimuli, n_dim):
@@ -78,7 +80,7 @@ def points_to_params(points):
     return params
 
 
-def calculate_ll(counts, probs, num_repeats, epsilon):
+def calculate_ll(counts, probs, num_repeats):
     reverse_counts = num_repeats - counts
     reverse_probs = 1 - probs
 
@@ -92,19 +94,19 @@ def calculate_ll(counts, probs, num_repeats, epsilon):
         model_bad = True
 
     # make sure there are no zero probabilities (avoid log(0) error)
-    probs[prob_zero] += epsilon
+    probs[prob_zero] += EPSILON
     log_likelihood = counts.dot(log2(probs))
     return log_likelihood, model_bad
 
 
-def dist_model_ll_vectorized(pair_a, pair_b, judgment_counts, params, stimuli):
+def dist_model_ll_vectorized(pair_a, pair_b, judgment_counts, judgment_repeats, params, stimuli):
     """ Get the likelihood using probabilities from erf geometry, i.e. the geometry
     that takes into account noise as Gaussian sources. """
     # get geometry probabilities and join counts and geometry prob for each trial (N, p)
     interstimulus_distances = squareform(pdist(stimuli))
     probs = find_probabilities(interstimulus_distances, pair_a, pair_b, params['noise_st_dev'], params['no_noise'])
     # calculate log-likelihood, is_bad flag
-    return calculate_ll(judgment_counts, probs, params['num_repeats'], params['epsilon'])
+    return calculate_ll(judgment_counts, probs, judgment_repeats)
 
 
 def find_probabilities(distances, pair_a, pair_b, noise_st_dev, no_noise=False):
@@ -125,31 +127,34 @@ def find_probabilities(distances, pair_a, pair_b, noise_st_dev, no_noise=False):
     return probabilities
 
 
-def random_choice_ll(judgments, params):
+def random_choice_ll(judgments, judgment_repeats):
     """ LL is calculated as sum over trials of N(i>j)*P(i>j).
     In this case the P(i> j) for any i or j is 0.5.
     So we sum N*0.5 """
     counts = []
     probs = []
-    for v in judgments.values():
+    repeats = []
+    for (k, v) in judgments.items():
         counts.append(v)
         probs.append(0.5)
-    return calculate_ll(np.array(counts), np.array(probs), params['num_repeats'], params['epsilon'])
+        repeats.append(judgment_repeats[k])
+    return calculate_ll(np.array(counts), np.array(probs), np.array(repeats))
 
 
-def best_model_ll(judgments, params):
+def best_model_ll(judgments, judgment_repeats):
     """ Use probabilities from observed judgements to calculate likelihood. So if i> j
     2/5 times, prob = 0.4. """
-    num_repeats = float(params['num_repeats'])
     counts = []
     probs = []
-    for v in judgments.values():
+    repeats = []
+    for (k, v) in judgments.items():
         counts.append(v)
-        probs.append(v / num_repeats)
-    return calculate_ll(np.array(counts), np.array(probs), num_repeats, params['epsilon'])
+        probs.append(v / judgment_repeats[k])
+        repeats.append(judgment_repeats[k])
+    return calculate_ll(np.array(counts), np.array(probs), np.array(repeats))
 
 
-def cost_of_model_fit(stimulus_params, pair_a, pair_b, judgment_counts, params):
+def cost_of_model_fit(stimulus_params, pair_a, pair_b, judgment_counts, judgment_repeats, params):
     """
     Stimulus_params is the independent variable. LL is the dependent variable.
     :param stimulus_params: nonzero coordinates of each of the stimuli stretched into one vector
@@ -162,7 +167,7 @@ def cost_of_model_fit(stimulus_params, pair_a, pair_b, judgment_counts, params):
     # get points from params
     points = params_to_points(stimulus_params, params['num_stimuli'], params['n_dim'])
     # calculate likelihood using distance geometry and given points
-    ll, is_bad = dist_model_ll_vectorized(pair_a, pair_b, judgment_counts, params, points)
+    ll, is_bad = dist_model_ll_vectorized(pair_a, pair_b, judgment_counts, judgment_repeats, params, points)
     LOG.debug('geometry is good: {}'.format(not is_bad))
     if is_bad:
         LOG.info("WARNING: This model is infeasible.")

analysis/simulation/run_v2.py
@@ -12,6 +12,7 @@ from scipy.spatial.distance import pdist, squareform
 import analysis.geometry.euclidean as model
 import analysis.simulation.experiment as exp
 import analysis.simulation.experiment_ranking as exp_ranking
+from analysis.simulation.simulate_coord_noise import run_experiment as coord_noise_run_exp
 import analysis.model_fitting.pairwise_likelihood_analysis as an
 import analysis.model_fitting.run_mds_seed as rs
 from analysis.geometry.hyperbolic import loid_map, hyperbolic_distances, sphere_map, spherical_distances
@@ -37,7 +38,7 @@ def simulate_judgments(parameters, points):
 
     if parameters['paradigm'] == 'pairwise_comparisons':
         judgments, points = exp.run_experiment(points, distances, parameters)
-    elif parameters['paradigm'] == 'simple_ranking':
+    elif parameters['paradigm'] == 'simple_ranking' and parameters['decision_noise_type'] != 'coordinates':
         data = exp_ranking.run_experiment(points, distances, parameters, simple_err_model=True)
         judgments = {}
         for trial, responses in data.items():
@@ -49,6 +50,10 @@ def simulate_judgments(parameters, points):
                 else:
                     judgments[pair] += judgment
                     judgments[pair] = judgments[pair] / float(2)
+    elif parameters['decision_noise_type'] == 'coordinates':
+        # simple ranking trials. Noise only at level of coords not dist or comparison.
+        print('simulating judgments with coord noise')
+        judgments = coord_noise_run_exp(points, parameters)
     else:
         raise NotImplementedError('Invalid paradigm')
     return judgments, distances
@@ -97,7 +102,7 @@ def main(true_dim, parameters, min_model=1, max_model=5, show=False):
     # calculate log-likelihood, is_bad flag
     probs = an.find_probabilities(
         interstim_distances, trial_pairs_a, trial_pairs_b,
-        parameters['sigmas'], parameters['no_noise'])
+        np.sqrt(parameters['sigmas']['dist']**2 + parameters['sigmas']['compare']**2), parameters['no_noise'])
     ll_groundtruth = an.calculate_ll(
         judgment_counts, probs, parameters['num_repeats'], parameters['epsilon'])[0] / float(
         num_trials * parameters['num_repeats'])
@@ -200,6 +205,7 @@ def helper(ii):
             'num_repeats': args.repetitions,
             'sigmas': {'compare': CONFIG['sigmas'], 'dist': 0},
             'paradigm': args.paradigm,
+            'decision_noise_type': args.decision_noise,
             'true_geometry': args.geometry,
             'curvature': CONFIG['curvature'],
             'dist_metric': 'euclidean',
@@ -223,21 +229,25 @@ if __name__ == '__main__':
     print(CONFIG)
     # ask for and parse user input
     parser = argparse.ArgumentParser()
-    parser.add_argument("-it", "--iterations", type=int, default=1)
+    parser.add_argument("-it", "--iterations", type=int, default=4)
     parser.add_argument("-n", "--num_stimuli", type=int, default=37)
-    parser.add_argument("-g", "--geometry", choices=["euclidean", "hyperbolic", "spherical"], default="spherical")
+    parser.add_argument("-g", "--geometry", choices=["euclidean", "hyperbolic", "spherical"], default="euclidean")
     parser.add_argument("-d1", "--min_model_dim", choices=[1, 2, 3, 4, 5], type=int, default=3)
-    parser.add_argument("-d2", "--max_model_dim", choices=[1, 2, 3, 4, 5], type=int, default=1)
-    parser.add_argument("-t1", "--min_true_dim", choices=[2, 3, 4, 5], type=int, default=2)
-    parser.add_argument("-t2", "--max_true_dim", choices=[2, 3, 4, 5], type=int, default=2)
+    parser.add_argument("-d2", "--max_model_dim", choices=[1, 2, 3, 4, 5], type=int, default=3)
+    parser.add_argument("-t1", "--min_true_dim", choices=[2, 3, 4, 5], type=int, default=3)
+    parser.add_argument("-t2", "--max_true_dim", choices=[2, 3, 4, 5], type=int, default=3)
     parser.add_argument("-r", "--repetitions", help="Number of times to repeat a trial", type=int, default=5)
     parser.add_argument("-s", "--sampling", choices=["uniform", "spherical_shell", "gaussian"],
                         default="gaussian"),
     parser.add_argument("-p", "--paradigm", choices=["pairwise_comparisons", "simple_ranking"],
                         default="simple_ranking")
+    parser.add_argument("-dn", "--decision_noise", choices=["comparison", "coordinates"],
+                        default="coordinates")
     args = parser.parse_args()
 
     LOG.info("Running simulation with arguments: {}".format(args))
+    if args.decision_noise == 'coordinates' and args.geometry != 'euclidean':
+        raise NotImplementedError('Coordinate level noise has only been implemented for Euclidean distance metric!')
 
     pool = Pool(1)
     iterations = range(args.iterations)

analysis/util.py
@@ -57,14 +57,15 @@ def all_distance_pairs(trial_key):
 def ranking_to_pairwise_comparisons(distance_pairs, ranked_stimuli):
     """ Convert ranking data to comparisons of pairs of pairs of stimuli
 
-    @param distance_pairs: kkk
+    @param distance_pairs:
     :type distance_pairs: list
-    :param ranked_stimuli: kj
+    :param ranked_stimuli:
     :type ranked_stimuli: list
     """
     # ranked_stimuli is a list of lists. each list is a 'repeat'
     rank = {}
     comparisons = {}
+    num_repeats = {}
     for stimulus_list in ranked_stimuli:
         for index in range(len(stimulus_list)):
             rank[stimulus_list[index]] = index
@@ -74,13 +75,15 @@ def ranking_to_pairwise_comparisons(distance_pairs, ranked_stimuli):
             stim2 = dists[1].split(',')[1]
             if pair not in comparisons:
                 comparisons[pair] = 1 if rank[stim1] < rank[stim2] else 0
+                num_repeats[pair] = 1
             else:
+                num_repeats[pair] += 1
                 if rank[stim1] < rank[stim2]:
                     comparisons[pair] += 1
-    return comparisons
+    return comparisons, num_repeats
 
 
-def judgments_to_arrays(judgments_dict):
+def judgments_to_arrays(judgments_dict, repeats):
     """Instead of having trials be a dictionary with keys made of tuples,
     convert judgment keys and values into numpy arrays for faster operations """
     # the indices of the stimuli for each trial's "first" pair of stimuli
@@ -88,7 +91,8 @@ def judgments_to_arrays(judgments_dict):
     # the indices of the stimuli for each trial's "second" pair of stimuli
     second_pair = np.array([np.array(trial[-1]) for trial in judgments_dict.keys()])
     comparison_counts = np.array([v for k, v in judgments_dict.items()], dtype='float')
-    return first_pair, second_pair, comparison_counts
+    comparison_repeats = np.array([repeats[k] for k, v in judgments_dict.items()], dtype='float')
+    return first_pair, second_pair, comparison_counts, comparison_repeats
 
 
 def reformat_key(comparison_trial_key):